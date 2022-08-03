ecca104c782b7657cc4e02a2c5f953efff89aa35
Trintu <devrymike@yahoo.com>
2022-08-03 15:02:14 -0500

    jenkins install

--- modules/jenkinstest/manifests/jenkinst.pp
+++ modules/jenkinstest/manifests/jenkinst.pp
@@ -5,9 +5,23 @@
 # @example
 #   include jenkinstest::jenkinst
 class jenkinstest::jenkinst {
-  file {'/tmp/hello':
-    ensure   => 'present',
-    content  => 'Hello World',
-    path     => '/tmp/hello',
+  exec { 'get-jenkins-key':
+    cwd      => '/usr/bin',
+    command  => 'wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -',
+  }
+  exec { 'get-sources-list':
+    command  => 'sudo sh -c \'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list\'',
+  }
+  exec { 'apt-update':
+    command  => 'sudo apt update',
+  }
+  exec { 'jenkins-install':
+    command  => 'sudo apt install jenkins',
+  }
+  exec { 'jenkins-start':
+    command  => 'sudo systemctl start jenkins',
+  }
+  exec { 'jenkins-status':
+    command  => 'sudo systemctl status jenkins > /opt/status.txt',
   }
 }
