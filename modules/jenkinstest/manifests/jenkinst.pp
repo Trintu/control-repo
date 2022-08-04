1fcc110c86ee93e7b54ee044ece906c2634f1b8f
Trintu <devrymike@yahoo.com>
2022-08-03 21:41:48 -0500

    jenkins install

--- modules/jenkinstest/manifests/jenkinst.pp
+++ modules/jenkinstest/manifests/jenkinst.pp
@@ -6,8 +6,8 @@
 #   include jenkinstest::jenkinst
 
 class jenkinstest::jenkinst (
-  String $keyurl = lookup('jenkinstest::jenkinsurls.keyurl')
-  String $sourceurl = lookup('jenkinstest::jenkinsurls.sourceurl')
+  $keyurl = lookup('jenkinstest::jenkinsurls.keyurl')
+  $sourceurl = lookup('jenkinstest::jenkinsurls.sourceurl')
 ){
   exec { 'get-jenkins-key':
     command     => "/usr/bin/wget -qq -O - $keyurl | sudo apt-key add -",
