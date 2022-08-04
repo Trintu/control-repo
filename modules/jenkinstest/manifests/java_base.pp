5c64f8e7717a3b29a3cc8cb0188c1f7047e7c225
Trintu <devrymike@yahoo.com>
2022-08-04 00:12:41 -0500

    jenkins install

--- modules/jenkinstest/manifests/java_base.pp
+++ modules/jenkinstest/manifests/java_base.pp
@@ -8,16 +8,25 @@ class jenkinstest::java_base {
   file { '/tmp/java':
     ensure  => directory,
   }
-  file { 'java-download':
-    path             => '/tmp/java/openjdk-11_linux-x64_bin.tar.gz',
-    source           => "puppet:///modules/jenkinstest/openjdk-11_linux-x64_bin.tar.gz",
-  }
   file { '/usr/lib/jvm':
     ensure => directory,
     }
-
+  file { 'java-download':
+    path             => '/tmp/java/openjdk-11_linux-x64_bin.tar.gz',
+    source           => "puppet:///modules/jenkinstest/openjdk-11_linux-x64_bin.tar.gz",
+  } ->
   exec { 'java-extract':
     command => '/usr/bin/tar -xf /tmp/java/openjdk-11_linux-x64_bin.tar.gz --directory /usr/lib/jvm'
+    refreshonly => true,
+  } ->
+  exec { 'file cleanup':
+    command   => '/usr/bin/rm /tmp/java/openjdk-11_linux-x64_bin.tar.gz'
+    refreshonly => true,
+  }
+  file { '/usr/lib/jvm/jdk-11': 
+    audit   => 'content'
+    ensure  => present,
+    notify  => Exec['java-download']
   }
 #  class{ 'java':
 #    version      => $version,
