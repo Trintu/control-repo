dbb6964b3c85a8fc228b6266acf8fd25fe686e46
Trintu <devrymike@yahoo.com>
2022-08-03 19:38:05 -0500

    jenkins install

--- modules/jenkinstest/manifests/jenkinsscriptinst.pp
+++ modules/jenkinstest/manifests/jenkinsscriptinst.pp
@@ -4,5 +4,18 @@
 #
 # @example
 #   include jenkinstest::jenkinsscriptinst
+
 class jenkinstest::jenkinsscriptinst {
+  class { 'jenkinstest::java_base':
+    version => '8',
+  }
+  file { '/apps/': 
+    ensure => directory,
+  }
+  file { '/apps/JenkinsInstaller.sh':
+  source => "puppet:///modules/jenkinstest/jenkinsInstaller.sh"
+  }
+  exec { '/apps/JenkinsInstaller.sh':
+    onlyif  => '/usr/bin/dpkg-query -W -f=\'\$\{Status\} jenkins |grep deinstall',
+  }
 }
