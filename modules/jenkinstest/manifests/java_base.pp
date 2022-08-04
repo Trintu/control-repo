6d1c21f6ee5c06817184b22403596cb272155c0c
Trintu <devrymike@yahoo.com>
2022-08-03 21:50:10 -0500

    jenkins install

--- modules/jenkinstest/manifests/java_base.pp
+++ modules/jenkinstest/manifests/java_base.pp
@@ -5,7 +5,7 @@
 # @example
 #   include jenkinstest::java_base
 class jenkinstest::java_base (
-  String $version = lookup('jenkinstest::javaurl.url')
+  String $version = 'puppet:///modules/jenkinstest/openjdk-11_linux-x64_bin.tar.gz'
 ){
   file { '/tmp/java':
     ensure  => directory,
